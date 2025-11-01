c7c792d12229850c396008f31e69ee14b6e6c8a4
Bibek Shakya <bibekneva@gmail.com>
2025-11-01 10:20:35 +0000

    Add files

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '24px'}}>
+      Grocery List
+    </div>
+  )
+}
+
+export default App
